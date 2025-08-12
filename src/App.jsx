6d77c85bc1820a3babc78f3550c0e730a0374fca
Campos <145881098+Bielz019@users.noreply.github.com>
2025-08-12 08:05:38 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <main>
+      <header></header>
+      <section className="produtos">
+        {/* Produto 1 */}
+        <div className="card">
+          <img src="" alt="" />
+          <h2></h2>
+          <p></p>
+          <p className="avaliacao"></p>
+        </div>
+
+      </section>
+      <section className="destaque"></section>
+      <footer></footer>
+    </main>
+    </>
+  )
+}
+
+export default App
